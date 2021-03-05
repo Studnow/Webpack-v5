3b7954bf571c5e11a119d964f378678ca9a10615
Igor <silent659@gmail.com>
2021-03-05 16:16:22 +0200

    add file-management (css/fonts/images), dev-server, code-splitting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+// import './style.css';
+// import Logo from './iamp-logo.svg';
+// import printMe from './print'
 
-function component() {
-  const element = document.createElement('div');
+// function component() {
+//   const element = document.createElement('div');
+//   const btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.classList.add('hello');
 
-  return element;
+//   const myLogo = new Image();
+//   myLogo.src = Logo;
+
+//   element.appendChild(myLogo);
+
+//   btn.innerHTML = 'Click me and check the console!';
+//   btn.onclick = printMe;
+
+//   element.appendChild(btn);
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+function getComponent() {
+  return import('lodash').then(({ default: _ }) => {
+    const element = document.createElement('div');
+
+    element.innerHTML = _.join(["Hello", 'webpack'], " ");
+
+    return element;
+  })
+    .catch((error) => 'An error occured while loading the component');
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+  document.body.appendChild(component);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,41 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  }
 };
