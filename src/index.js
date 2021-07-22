9ab2179ead484cde6807e85d02e0cca656b1a5ed
Igor <silent659@gmail.com>
2021-07-22 06:49:09 +0300

    add aos animation library

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ import "./styles/style.css";
 // js
 
 import Alpine from 'alpinejs'
+import AOS from "aos";
+import 'aos/dist/aos.css'
+
 // register Alpine extension code between import and start()
 
 window.Alpine = Alpine
- 
+
 Alpine.data('dropdown', () => ({
   open: false,
 
@@ -20,3 +23,6 @@ Alpine.data('dropdown', () => ({
 }))
 
 Alpine.start()
+AOS.init({
+  duration: 1200,
+});
